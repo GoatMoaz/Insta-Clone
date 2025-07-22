3bd05632ab4c74da9d79cd55ee46c5fada2886ed
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2025-07-22 16:09:45 +0300

    feat: implement logout functionality in Sidebar component

--- src/components/layouts/Sidebar/Logout.tsx
+++ src/components/layouts/Sidebar/Logout.tsx
@@ -1,8 +1,15 @@
 import { Box } from "@chakra-ui/react";
 import { RouterLink } from "@/config/RouterLink";
 import { BiLogOut } from "react-icons/bi";
+import { useUserStore } from "@/store/useUserStore";
 
 export const Logout = () => {
+  const logout = useUserStore((state) => state.logout);
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <RouterLink
       display={"flex"}
@@ -13,6 +20,7 @@ export const Logout = () => {
       borderRadius={6}
       p={2}
       justifyContent={{ base: "center", md: "flex-start" }}
+      onClick={handleLogout}
     >
       <BiLogOut size={25} />
       <Box display={{ base: "none", md: "block" }}>Log out</Box>
